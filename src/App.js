fe924c32fe9c3308d7b64646b32110354c8d5284
filip-x <58309376+filip-x@users.noreply.github.com>
2021-09-02 08:57:37 +0300

    ceva2
    cevaincerc2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './tictactoe/src/App';
 
 function App() {
   return (
+      <Game/>,
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
