120711e68bf248fea58eee080d36024491bafb85
filip-x <58309376+filip-x@users.noreply.github.com>
2021-09-02 23:28:45 +0300

    iar3
    iar3

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class Square extends React.Component {
     render() {
         return (
             <button className="square" onClick={ function (){console.log('click');}}>
-                {this.props.value()}
+                {this.props.value}
             </button>
         );
     }
