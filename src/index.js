47d3d6a1395bf40e5c752d46ff4e8a5944c5567f
filip-x <58309376+filip-x@users.noreply.github.com>
2021-09-02 09:08:09 +0300

    ceva3
    ceva3

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Game/>
   </React.StrictMode>,
   document.getElementById('root')
 );
